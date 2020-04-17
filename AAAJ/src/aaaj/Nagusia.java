77c278441cd9d136a7098ce2508da99769499dc6
anderra57 <49227774+anderra57@users.noreply.github.com>
2020-04-17 03:57:07 +0200

    datu basearen zerbitzaria eguneratu

--- AAAJ/src/aaaj/Nagusia.java
+++ AAAJ/src/aaaj/Nagusia.java
@@ -5,9 +5,9 @@ import java.sql.SQLException;
 
 public class Nagusia {
 	
-	static String zerbitzaria= "jdbc:mysql://remotemysql.com:3306/fOZj7JO0X4";
-	static String erabiltzailea= "fOZj7JO0X4"; // mysql
-	static String pasahitza= "eHiAgxvLE4"; // mysql
+	static String zerbitzaria= "jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7333994";
+	static String erabiltzailea= "sql7333994"; // mysql
+	static String pasahitza= "DdkjPEkHYL"; // mysql
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
