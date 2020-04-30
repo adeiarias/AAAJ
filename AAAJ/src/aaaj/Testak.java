23db32d7b72f15e5e661fc68fcadffe0353f29ce
anderra57 <49227774+anderra57@users.noreply.github.com>
2020-04-30 23:14:51 +0200

    testak bukatu

--- AAAJ/src/aaaj/Testak.java
+++ AAAJ/src/aaaj/Testak.java
@@ -1,15 +1,10 @@
 package aaaj;
-
 import static org.junit.Assert.*;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -29,15 +24,6 @@ public class Testak {
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		konexioa.close();
-	}
-
-	@Before
-	public void setUp() throws Exception {
-		
-	}
-
-	@After
-	public void tearDown() throws Exception {
 	}
 	
 	/*
@@ -78,6 +64,179 @@ public class Testak {
 		assertFalse(rs.next());
 	}
 	
-	
+	/*
+	 * BEZERO JAKIN BATEN STOCKA IKUSI
+	 */
 
+	@Test
+	public void BezeroStockIkusi() throws SQLException {
+		
+		// BEZERO:
+		rs=stm.executeQuery("SELECT stock.pkode,stock.kantitate,produktu.izena,produktu.deskribapena FROM (stock JOIN bezero ON stock.bkode=bezero.bkode) JOIN produktu ON stock.pkode=produktu.pkode where stock.bkode=1;");
+		assertTrue(rs.next());
+		
+	}
+
+	/*
+	 * ENTREGATU BEHARREKO ESKAERA GUZTIAK IKUSI
+	 */
+
+	@Test
+	public void EntregatuBeharrekoEskaeraGuztiakIkusi() throws SQLException {
+
+		rs=stm.executeQuery("SELECT eskaera.id, bezero.bkode, bezero.dendaizena,bezero.helbide,bezero.tlf FROM eskaera JOIN bezero ON eskaera.bkode=bezero.bkode where eskaera.entregatuta=false;");
+		assertTrue(rs.next());
+
+	}
+	
+	/*
+	 *  ESKAERAK/BEZEROAK/GARRAIATZAILEAK GUNEKA FILTRATU
+	 */
+
+	@Test
+	public void GunekaFiltratu() throws SQLException {
+		
+		//BEZERO:
+		rs=stm.executeQuery("SELECT bkode, dendaizena, helbide, tlf, ifz FROM bezero WHERE gune=159;");
+		assertFalse(rs.next());
+		
+		//(ESKAERA):
+		rs=stm.executeQuery("SELECT eskaera.* FROM eskaera JOIN bezero ON eskaera.bkode=bezero.bkode WHERE gune=154;");
+		assertFalse(rs.next());
+		
+		//GARRATZAILE:
+		rs=stm.executeQuery("SELECT gkode, izena, abizena, tlf, nan FROM garraiatzaile WHERE gune=205;");
+		assertFalse(rs.next());
+	}
+	
+	/*
+	 *  ESKAERA ID-Z BILATU
+	 */
+
+	@Test
+	public void EskaeraID() throws SQLException {
+		
+		rs=stm.executeQuery("SELECT bezero.bkode, bezero.dendaizena, eskatu.pkode, produktu.izena, produktu.deskribapena, eskatu.kantitate FROM ((eskatu JOIN eskaera ON eskatu.id=eskaera.id) JOIN produktu ON eskatu.pkode=produktu.pkode) JOIN bezero on eskaera.bkode=bezero.bkode WHERE eskatu.id=17890178900987;");
+		assertFalse(rs.next());	
+		
+	}
+	
+	/*
+	 *  ESKAEREN HISTORIALA BISTARATU 
+	 */
+
+	@Test
+	public void EskaerenHistoriala() throws SQLException {
+		
+		rs=stm.executeQuery("SELECT eskaera.id, bezero.bkode, bezero.dendaizena, eskatu.pkode, produktu.izena, produktu.deskribapena, eskatu.kantitate, eskaera.entregatuta FROM ((eskatu JOIN eskaera ON eskatu.id=eskaera.id) JOIN produktu ON eskatu.pkode=produktu.pkode) JOIN bezero ON eskaera.bkode=bezero.bkode;");
+		assertTrue(rs.next());
+		
+	}
+	
+	/*
+	 *	BEZERO/GARRAIATZAILEAREN INFORMAZIOA BISTARATU 
+	 */
+
+	@Test
+	public void InformazioaBistaratu() throws SQLException {
+		
+		//BEZERO:
+		rs=stm.executeQuery("select * from bezero where bkode=1;");
+		assertTrue(rs.next());
+		
+		//GARRAIATZAILE: 
+		rs=stm.executeQuery("select * from garraiatzaile where gkode=1;");
+		assertTrue(rs.next());
+		
+	}
+	
+	/*
+	 *	ORGAN SARTU
+	 */
+
+	@Test
+	public void OrganSartu() throws SQLException {
+		
+		stm.executeUpdate("INSERT INTO eskatu VALUES ('1','1', 1)");
+		rs=stm.executeQuery("SELECT * FROM eskaera where id=1");
+		assertTrue(rs.next());	
+		
+	}
+	
+	/*
+	 *	ORGA HUSTU 
+	 */
+
+	@Test
+	public void OrgaHustu() throws SQLException {
+		
+		stm.executeUpdate("DELETE FROM eskaera WHERE id=1");
+		rs=stm.executeQuery("SELECT * FROM eskaera where id=1");
+		assertFalse(rs.next());
+		
+	}
+	
+	/*
+	 *	ESKAERA EZEZTATU
+	 */
+
+	@Test
+	public void EskaeraEzeztatu() throws SQLException {
+		
+		stm.executeUpdate("DELETE FROM eskaera WHERE id=2 AND entregatuta=false");
+		rs=stm.executeQuery("SELECT * FROM eskaera where id=2");
+		assertFalse(rs.next());
+		
+	}
+	
+	/*
+	 *	PRODUKTUEN LISTA IKUSI
+	 */
+
+	@Test
+	public void ProduktuakIkusi() throws SQLException {
+		
+		rs=stm.executeQuery("SELECT * FROM produktu");
+		assertTrue(rs.next());
+		
+	}
+	
+	/*
+	 *	PRODUKTUA STOCK-EAN SARTU
+	 */
+
+	@Test
+	public void ProduktuaStock() throws SQLException {
+		
+		stm.executeUpdate("INSERT INTO stock VALUES (145678,6789,1)");
+		rs=stm.executeQuery("SELECT * FROM stock where eskBkode=145678");
+		assertFalse(rs.next());
+		
+	}
+	
+	/*
+	 *	GARRAIATZAILE TELEFONOA ALDATU
+	 */
+
+	@Test
+	public void GarraiatzaileAldatu() throws SQLException {
+		
+		//GARRAIATZAILE
+		stm.executeUpdate("UPDATE `garraiatzaile` SET `tlf` =  666666666 WHERE `garraiatzaile`.`gkode`=1");
+		rs=stm.executeQuery("SELECT * FROM garratzaile where tlf=666666666");
+		assertTrue(rs.next());
+		
+	}
+	
+	/*
+	 *	LISTAN DAGO
+	 */
+
+	@Test
+	public void IDListanDago() throws SQLException {
+
+		rs=stm.executeQuery("SELECT eskaera.bkode FROM eskatu JOIN eskaera ON eskatu.id = eskaera.id where eskatu.id =132469098630");
+		assertFalse(rs.next());
+		
+	}
 }
